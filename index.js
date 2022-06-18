94a83976b68dd4da34405d71cee84547bfa17b5b
Prajeetha R <82923980+Prajeetha313@users.noreply.github.com>
2022-06-18 21:39:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-var express=require("express")
-var app=express()
-app.listen(2000,()=>{
+var express = require("express")
+var app = express()
+const connectDB = require('./config/db')
+const authRoute = require("./routes/auth")
+
+require('dotenv').config();
+
+app.use(function(req, res, next){
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    next();
+})
+
+
+app.use(express.static(__dirname))
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({limit:'50mb'}));
+app.use(bodyParser.urlencoded({limit:'50mb', extended:true}));
+
+
+app.use("/auth", (authRoute));
+
+app.listen(process.env.PORT,()=>{
     console.log("server is listening")
 })
