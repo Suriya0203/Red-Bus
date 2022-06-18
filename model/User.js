fb13331f250ffca8392bcf79e0b05b4f32516d29
Prajeetha R <82923980+Prajeetha313@users.noreply.github.com>
2022-06-18 21:50:38 +0530

    Update User.js

--- model/User.js
+++ model/User.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('User', {
     name : {
-		type : String, 
-		required : true, 
+	type : String, 
+	required : true, 
     }, 
     email : {
         type : String, 
