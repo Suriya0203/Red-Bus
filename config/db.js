44c5a61d961dca552af58aad1f1536815328edb0
Prajeetha R <82923980+Prajeetha313@users.noreply.github.com>
2022-06-18 18:17:25 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 mongoose
      .connect(process.env.DB, 
      { useNewUrlParser: true, useUnifiedTopology: true })
-     .then(() => console.log( 'Database Connected',mongoose.connection.host,mongoose.connection.db ))
-     .catch(err => console.log( err ));
+     .then(() => console.log('Database Connected', mongoose.connection.host,mongoose.connection.db))
+     .catch(err => console.log(err));
 
 
 module.exports=mongoose;
