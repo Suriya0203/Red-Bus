9f123d73e7379dc4614045a79e7c51011fb950c4
Prajeetha R <82923980+Prajeetha313@users.noreply.github.com>
2022-07-10 22:53:47 +0530

    user part

--- client/src/reducer/bus.js
+++ client/src/reducer/bus.js
@@ -1,7 +1,10 @@
 import {
     VIEW_BUS_SUCCESSFULLY,
     VIEW_BUS_FAILURE,
-  
+    CREATE_TRIP_FAILURE,
+    CREATE_TRIP_SUCCESSFULL, 
+    GET_BUS_SUCCESSS, 
+    GET_BUS_FAILURE
     } from '../actions/type'
     
     const initialState = {
@@ -27,7 +30,23 @@ import {
             error: action.payload,
             bus: []
           };
- 
+          case CREATE_TRIP_SUCCESSFULL:
+            alert("Trip created successfully")
+            window.location.reload(false)
+        case GET_BUS_SUCCESSS : 
+          return {
+            ...state, 
+            loading: false, 
+            error : false, 
+            bus : action.payload
+          };
+        case GET_BUS_FAILURE : 
+          return {
+            ...state, 
+            loading : false, 
+            error : action.payload, 
+            bus : []
+          };
         default:
         
           return state;
