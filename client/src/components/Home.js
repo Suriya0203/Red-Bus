82901d78b84126355d51b8cdb9d3682222a30a98
Prajeetha R <82923980+Prajeetha313@users.noreply.github.com>
2022-07-15 19:14:58 +0530

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,25 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {useDispatch} from 'react-redux'
 import NavBar from './NavBar'
 import BackGroundImage from '../images/redbus.avif';
+import {connect} from 'react-redux';
 import {
     makeStyles 
 } from "@material-ui/core";
 import { IoSwapHorizontal } from 'react-icons/io5';
+import {IoMdSwap} from 'react-icons/io'
 import { getTrip } from '../actions/auth';
+import axios from 'axios';
 const useStyles = makeStyles((theme) => ({
     image : {
         width : "1300px",
-        height : '500px',
-        paddingLeft : '50px'
+        height : '400px',
+        margin :'0 auto'
+    }, 
+    suggestion : {
+        cursor : 'pointer', 
+        border :'none',
+        // borderRight : '1px solid black', 
+        // borderLeft : '1px solid black', 
+        // borderBottom : '1px solid black', 
+        '&:hover':{
+            backgroundColor:'#C1C1C1', 
+            border : 'none',
+            borderRadius: '5px'
+        }
     }
 }))
 
-const Home = () => {
+const Home = ({user,type}) => {
     const classes = useStyles();
     const [source, setSource] = useState("");
     const [destination, setDestination] = useState("");
     const [date, setDate] = useState();
+
+    const [availSource, setAvailSource] = useState([]);
+    const [availDestination, setAvailDestination] = useState([]);
+    const [sourceSuggestion, setSourceSuggestion] = useState([]);
+    const [destinationSuggestion, setDestinationSuggestion] = useState([])
 
     const dispatch = useDispatch();
     const handleSearchSubmit = (e) =>{
@@ -34,10 +54,68 @@ const Home = () => {
         setDestination(source);
     }
 
+    const onSourceSuggestionHandler = (text)=>{
+        setSource(text);
+        setSourceSuggestion([]);
+    }
+
+    const onDestinationSuggestionHandler = (text) => {
+        setDestination(text);
+        setDestinationSuggestion([]);
+    }
+
+    const handleSetSource = (text) => {
+        let matches = []
+        if(text.length > 0) {
+            matches = availSource.filter(availSources => {
+                const regex = new RegExp(`${text}`, "gi");
+                return availSources.departureLocation.match(regex)
+            })
+        }
+        console.log("matches1", matches)
+        setSourceSuggestion(matches)
+        setSource(text)
+    }  
+
+    const handleSetDestination = (text) => {
+        let matches = []
+        if(text.length > 0) {
+            matches = availDestination.filter(availDestinations => {
+                const regex = new RegExp(`${text}`, "gi");
+                return availDestinations.arrivalLocation.match(regex)
+            })
+        }
+        console.log("matches2", matches)
+        setDestinationSuggestion(matches)
+        setDestination(text);
+    }
+
+    useEffect(()=>{
+        const loadSources = async()=> {
+            const response = await axios.get("http://localhost:5000/trip/getSource")
+            console.log(response.data);
+            setAvailSource(response.data.data);
+        }
+        const loadDestination = async() => {
+            const response = await axios.get("http://localhost:5000/trip/getDestination")
+            console.log(response.data);
+            setAvailDestination(response.data.data);
+        }
+        loadSources();
+        loadDestination();
+
+
+    }, [])
+
   return (
     <div>
         <NavBar />
-        <img style={{width:"1200px"}} src={BackGroundImage} className={classes.image} />
+        <img src={BackGroundImage} className={classes.image} />
+        {
+        (user !== null)
+        
+        ?
+
         <div>
             <div
                 style={{
@@ -62,11 +140,16 @@ const Home = () => {
                 }}
                 type="text"
                 value={source} 
-                onChange={(e)=>setSource(e.target.value)} 
+                onChange={e=>handleSetSource(e.target.value)}
+                // onChange={(e)=>setSource(e.target.value)} 
                 placeholder = "Source"
             />  
-
-            <IoSwapHorizontal style={{fontSize: '30px', paddingLeft: '20px', paddingRight:'20px'}} onClick={() => swap()} />
+            <a style={{paddingLeft: '20px', paddingRight:'20px'}}><IoMdSwap onClick={() => swap()} /></a>
+            {/* <IoMdSwap style={{
+                fontSize: '30px', paddingLeft: '20px', paddingRight:'20px'
+                }} 
+                
+            /> */}
 
             <input 
                 style={{
@@ -80,7 +163,8 @@ const Home = () => {
                 }}
                 type="text"
                 value={destination} 
-                onChange={(e)=>setDestination(e.target.value)} 
+                onChange = {e => handleSetDestination(e.target.value)}
+                // onChange={(e)=>setDestination(e.target.value)} 
                 placeholder = "Destination"
             />
 
@@ -114,11 +198,62 @@ const Home = () => {
                 Search
             </button>
             </div>
-            
+            {/* <span style={{paddingLeft:'250px'}}></span> */}
+            {sourceSuggestion && sourceSuggestion.map((suggestion, i)=>
+                <div key = {i} className = {classes.suggestion}
+                    style={{
+                        // fontSize: '20px',
+                        // textAlign: 'center',
+                        width:'170px',
+                        marginLeft : '223px',
+                        top:'0',
+                        height : '30px',
+                        paddingLeft : '30px',
+                        paddingTop:'20px', 
+                        // border : 'none',
+                        fontFamily: 'Courier New, Courier, monospace', 
+                        // fontWeight: "bold", 
+                    }}
+                    onClick={()=>onSourceSuggestionHandler(suggestion.departureLocation)}
+                >{suggestion.departureLocation}</div>
+                )
+            }
+
+            {destinationSuggestion && destinationSuggestion.map((suggestion, i)=>
+                <div key = {i} className = {classes.suggestion}
+                    style={{
+                        // fontSize: '20px',
+                        // textAlign: 'center',
+                        width:'170px',
+                        height : '30px',
+                        marginLeft : '480px',
+                        paddingLeft : '30px',
+                        paddingTop:'20px',
+                        top:'0',
+                        // border : 'none',
+                        fontFamily: 'Courier New, Courier, monospace', 
+                        // fontWeight: "bold", 
+                    }}
+                    onClick={()=>onDestinationSuggestionHandler(suggestion.arrivalLocation)}
+                >{suggestion.arrivalLocation}</div>
+                )
+            }
         </div>
+
+        :
+        <span>
+
+        </span>
+
+        }
     </div>
     
   )
 }
-
-export default Home
+const mapStateToProps=state=>{
+    return {
+        user:state.auth.token,
+        type:state.auth.user
+    }
+  }
+export default connect(mapStateToProps)(Home)
