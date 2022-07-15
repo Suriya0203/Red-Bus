5502a2dc5662f43ae7cb93edbae5c624c1091ca6
Prajeetha R <82923980+Prajeetha313@users.noreply.github.com>
2022-07-15 18:51:27 +0530

    Update bookings.js

--- client/src/reducer/bookings.js
+++ client/src/reducer/bookings.js
@@ -1,42 +1,44 @@
 import {
  
-    GET_BOOKING_SUCCESS,
-    GET_BOOKING_FAILURE,
-    CREATE_BOOKING_FAILURE,
-    CREATE_BOOKING_SUCCESS,
-    GET_MY_BOOKING_FAILURE,
-    GET_MY_BOOKING_SUCCESS,
-    FETCH_TICKET_SUCCESS
-    } from '../actions/type'
-    
-    const initialState = {
-      booking: [],
-      loading: false,
-      error: null
-    };
-    
-    export default function productReducer(state = initialState, action) {
-      switch(action.type) {
-        case GET_BOOKING_SUCCESS : 
-          return {
-            ...state, 
-            loading: false, 
-            error : false, 
-            booking : action.payload
-          };
-        case GET_BOOKING_FAILURE : 
+  GET_BOOKING_SUCCESS,
+  GET_BOOKING_FAILURE,
+  CREATE_BOOKING_FAILURE,
+  CREATE_BOOKING_SUCCESS,
+  GET_MY_BOOKING_FAILURE,
+  GET_MY_BOOKING_SUCCESS,
+  FETCH_TICKET_SUCCESS
+  } from '../actions/type'
+  
+  const initialState = {
+    booking: [],
+    loading: false,
+    error: null
+  };
+  
+  export default function productReducer(state = initialState, action) {
+    switch(action.type) {
+      case GET_BOOKING_SUCCESS : 
+        return {
+          ...state, 
+          loading: false, 
+          error : false, 
+          booking : action.payload
+        };
+      case GET_BOOKING_FAILURE : 
+        return {
+          ...state, 
+          loading : false, 
+          error : action.payload, 
+          booking : []
+        };
+       case CREATE_BOOKING_SUCCESS:
           return {
             ...state, 
             loading : false, 
-            error : action.payload, 
+            error : false,
             booking : []
           };
-         case CREATE_BOOKING_SUCCESS:
-            alert("Ticket Booked Successfully");
-            window.location.reload(false);
-        case CREATE_BOOKING_FAILURE :
-            alert("User already booked");
-        case GET_MY_BOOKING_SUCCESS:
+          case GET_MY_BOOKING_SUCCESS:
           return {
             ...state, 
             loading: false, 
@@ -50,14 +52,25 @@ import {
             error : action.payload, 
             booking : []
           };
-        case  FETCH_TICKET_SUCCESS:
+
+          // alert("Ticket Booked Successfully");
+         
+          case CREATE_BOOKING_FAILURE :
+            return {
+              ...state, 
+              loading : false, 
+              error : true, 
+              booking : []
+            };
+            case  FETCH_TICKET_SUCCESS:
           return {
             ...state, 
             loading: false, 
             error : false, 
             booking : action.payload
           };
-        default:
-        
-          return state;
-      }}
+          // alert("User already booked");
+      default:
+      
+        return state;
+    }}
