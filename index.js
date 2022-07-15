f788a6942e6415d1f9bb6863cd91237298460650
Suriya0203 <suriya0203@gmail.com>
2022-07-15 20:19:51 +0530

    make it better

--- index.js
+++ index.js
@@ -33,7 +33,12 @@ app.use("/auth", (authRoute));
 app.use("/bus", (busroute));
 app.use("/trip", (triproute));
 app.use("/booking",(Bookings))
-app.use(express.static(path.resolve(__dirname, "./client")))
-app.listen(process.env.PORT,()=>{
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    });
+   }
+app.listen(process.env.PORT||5000,()=>{
     console.log("server is listening")
 })
