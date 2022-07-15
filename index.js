44d47c66038f53eeb4ebed4ddbf8ac5cc74a1cb3
Suriya0203 <suriya0203@gmail.com>
2022-07-15 20:51:34 +0530

    first commit

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ app.use("/bus", (busroute));
 app.use("/trip", (triproute));
 app.use("/booking",(Bookings))
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static('client/build'));
+    app.use(express.static('./client/build'));
     app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join(__dirname + './client/build/index.html'));
     });
    }
 app.listen(process.env.PORT||5000,()=>{
