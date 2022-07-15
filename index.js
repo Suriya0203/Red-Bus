0cbffc5abcc02889b0ee3767eabffb1d9b228c52
Suriya0203 <suriya0203@gmail.com>
2022-07-15 20:57:08 +0530

    first commit

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging')
     res.sendFile(path.join(__dirname + './client/build/index.html'));
     });
    }
+app.use("/",express.static(path.join(__dirname,'./client/build')))
 app.listen(process.env.PORT||5000,()=>{
     console.log("server is listening")
 })
