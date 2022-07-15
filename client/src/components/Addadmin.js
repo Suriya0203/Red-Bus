7ecaa1ee69a3ec164043a92020e27872928b56a8
Prajeetha R <82923980+Prajeetha313@users.noreply.github.com>
2022-07-15 19:10:25 +0530

    Update Addadmin.js

--- client/src/components/Addadmin.js
+++ client/src/components/Addadmin.js
@@ -1,9 +1,8 @@
-// import { model } from "mongoose";
-import React,{ useState } from "react"
+import React,{ useEffect, useState } from "react"
 import './Addbuss.css'
 import NavBar from './NavBar'
-import { Addadmin_action } from "../actions/auth";
-import { connect } from "react-redux";
+import { Addadmin_action, loadUser } from "../actions/auth";
+import { connect, } from "react-redux";
 function Addadmin({Addadmin_action}){
     const [formData, setFormData] = useState({
       name: "",
@@ -11,7 +10,7 @@ function Addadmin({Addadmin_action}){
       password:"",
       age:"",
       gender:"",
-      is_admin:"true"
+      is_admin:"yes"
         
 	});
     const { name, email,password, age,gender,is_admin} = formData;
@@ -20,64 +19,95 @@ function Addadmin({Addadmin_action}){
     setFormData({ ...formData, [e.target.name]: e.target.value });
     const onSubmit = async (e) => {
 		console.log("suriya")
-        // setFormData({ ...formData, [is_admin]:"true"});
-		e.preventDefault();
+    e.preventDefault();
 		Addadmin_action(formData)
-	
-
 	}
+
+  useEffect(()=>{
+    loadUser();
+  })
+
+
     return (
 <div>
-<NavBar /><br/><br/><br/><br/> 
-{/* <h1 style={{
-    textAlign:"center"
-}}>ADD BUS</h1> */}
-<h1 style={{
-    textAlign:"center"
-}}>ADD ADMIN</h1>
-<div class="addbus">
-{/* <NavBar /> */}
-<form onSubmit={(e) => onSubmit(e)}>
-  <label for="name">name</label>
-  <input type="text" id="name" name="name" 
-  value={name}
-  onChange={(e) => onChange(e)}required
-  
-  placeholder="Enter Name.."/>
+  <NavBar /><br/><br/><br/><br/> 
+  <div style={{width:'50%', margin:'0 auto', color:'grey'}}>
 
-  <label for="email">email</label>
-  <input type="text" id="email" name="email" 
-  value={email}
-  onChange={(e) => onChange(e)}
-  required
-  placeholder="Enter email.."/>
 
-  <label for="password">password</label>
-  <input type="text" id="password" name="password" 
-   value={password}
-   onChange={(e) => onChange(e)}required
-  placeholder="Enetr Password.."/>
-  <label for="age">age</label>
-  <input type="number" id="age" name="age"
-   value={age}required
-   onChange={(e) => onChange(e)}
-    placeholder="Enetr Age.."/>
-    <label for="gender">gender</label>
-  <input type="text" id="gender" name="gender"
-   value={gender}required
-   onChange={(e) => onChange(e)}
-    placeholder="Enetr Gender.."/>
-  {/* <label for="country">Country</label>
-  
-  <select id="country" name="country">
-    <option value="australia">Australia</option>
-    <option value="canada">Canada</option>
-    <option value="usa">USA</option>
-  </select> */}
+  <h1 style={{
+      textAlign:"center", color:'grey'
+  }}>ADD ADMIN</h1>
 
-  <input type="submit" value="Submit"/>
-</form>
-</div></div>
+      <div>
+        <form onSubmit={(e) => onSubmit(e)}>
+
+          <label for="name">Name</label>
+          <input 
+                type="text" 
+                id="name"
+                name="name" 
+                value={name}
+                onChange={(e) => onChange(e)}
+                required
+                placeholder="Enter Name"
+          />
+
+          <label for="email">Email</label>
+          <input 
+                type="text" 
+                id="email" 
+                name="email" 
+                value={email}
+                onChange={(e) => onChange(e)}
+                required
+                placeholder="Enter email"
+          />
+
+          <label for="password">Password</label>
+          <input 
+                type="text" 
+                id="password" 
+                name="password" 
+                value={password}
+                onChange={(e) => onChange(e)}required
+                placeholder="Enter Password"
+          />
+
+          <label for="age">Age</label>
+          <input 
+                type="number" 
+                id="age" 
+                name="age"
+                style={{
+                  width: '100%',
+                    padding: '12px 20px',
+                    margin: '8px 0',
+                    display: 'inline-block',
+                    border: '1px solid #ccc',
+                    borderRadius: '4px',
+                    boxSizing: 'border-box'
+                }}
+                value={age}required
+                onChange={(e) => onChange(e)}
+                placeholder="Enter Age"
+          />
+
+          <label for="gender">Gender</label>
+          <input 
+                type="text" 
+                id="gender" 
+                name="gender"
+                value={gender}required
+                onChange={(e) => onChange(e)}
+                placeholder="Enter Gender"
+          />
+          
+          <input type="submit" value="Submit"/>
+
+        </form>
+      </div>
+    </div>
+</div>
 
     );
 }
