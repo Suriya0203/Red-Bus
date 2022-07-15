c20663f738d6dbde4c4e6c93e4a5108136c8d336
Prajeetha R <82923980+Prajeetha313@users.noreply.github.com>
2022-07-15 18:19:55 +0530

    Delete Helpers directory

--- Helpers/BookingValidation.js
+++
@@ -1,7 +0,0 @@
-const Joi=require('joi')
-
-const authSchema=Joi.object({
-    //email:Joi.string().email().lowercase().required(),
-    //phoneNumber:Joi.string().length(10).pattern(/^[0-9]+$/).required(),
-})
-module.exports=authSchema;
--- Helpers/Validation.js
+++
@@ -1,7 +0,0 @@
-const Joi=require('joi')
-
-const authSchema=Joi.object({
-    email:Joi.string().email().lowercase().required(),
-    password:Joi.string().min(5).required(),
-})
-module.exports=authSchema;
