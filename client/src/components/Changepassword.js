89ef7fd36f7ea86891c33109a5f5c44260da34c1
Prajeetha R <82923980+Prajeetha313@users.noreply.github.com>
2022-07-15 19:13:25 +0530

    Update Changepassword.js

--- client/src/components/Changepassword.js
+++ client/src/components/Changepassword.js
@@ -10,7 +10,6 @@ const ChangePassword = ({ChangePassword_action}) => {
 		ReEnterPassword:""
         
 	});
-
 	const { CurrentPassword, NewPassword,ReEnterPassword } = formData;
 
 	const onChange = (e) =>
@@ -22,68 +21,105 @@ const ChangePassword = ({ChangePassword_action}) => {
 		if (NewPassword != ReEnterPassword) {
 			console.log("password not match")
 			alert("password doesn't match")
-	}
-	else{
-		ChangePassword_action(formData)
-	}
-
+		}
+		else{
+			ChangePassword_action(formData)
+		}
 	}
 	
-
+	if(localStorage.getItem("token")) {
 	return (
 		<div>
 		<NavBar /><br/><br/><br/><br/>
-		<div className="login-form">
-			<br />
-			<h1 className="heading" style={{
-				textAlign:"center"
-			}}>ChangePassword</h1><br/>
-			<form className="form" onSubmit={(e) => onSubmit(e)}>
-				<div className="form-group">
+			<div className="login-form" style={{width:'40%', margin:'0 auto'}}>
+				<br />
+				<h3 
+					className="heading" 
+					style={{
+						textAlign:"center", 
+						color:'grey'
+					}}>
+						Change Password
+				</h3><br/>
+				<form className="form" onSubmit={(e) => onSubmit(e)}>
+
+					<div className="form-group">
+						<input
+							type="password"
+							placeholder="Current Password"
+							name="CurrentPassword"
+							value={CurrentPassword}
+							onChange={(e) => onChange(e)}
+							style={{
+								width: '100%',
+								padding: '12px 20px',
+								margin: '8px 0',
+								display: 'inline-block',
+								border: '1px solid #ccc',
+								borderRadius: '4px',
+								boxSizing: 'border-box'
+							}}
+							required
+						/>
+					</div>
+
+					<div className="form-group">
+						<input
+							type="password"
+							placeholder="New Password"
+							name="NewPassword"
+							minLength="5"
+							value={NewPassword}
+							onChange={(e) => onChange(e)}
+							required
+							style={{
+								width: '100%',
+								padding: '12px 20px',
+								margin: '8px 0',
+								display: 'inline-block',
+								border: '1px solid #ccc',
+								borderRadius: '4px',
+								boxSizing: 'border-box'
+							}}
+						/>
+					</div>
+
+					<div className="form-group">
+						<input
+							type="password"
+							placeholder="Conform New Password"
+							name="ReEnterPassword"
+							minLength="5"
+							value={ReEnterPassword}
+							onChange={(e) => onChange(e)}
+							required
+							style={{
+								width: '100%',
+								padding: '12px 20px',
+								margin: '8px 0',
+								display: 'inline-block',
+								border: '1px solid #ccc',
+								borderRadius: '4px',
+								boxSizing: 'border-box'
+							}}
+						/>
+					</div>
 					
-					<input
-						type="password"
-						placeholder="Current Password"
-						name="CurrentPassword"
-						value={CurrentPassword}
-						onChange={(e) => onChange(e)}
-						required
-					
-					/>
-				</div>
-				<div className="form-group">
-					<input
-						type="password"
-						placeholder="New Password"
-						name="NewPassword"
-						minLength="5"
-						value={NewPassword}
-						onChange={(e) => onChange(e)}
-						required
-						
-					/>
-				</div>
-				<div className="form-group">
-					<input
-						type="password"
-						placeholder="Re EnterPassword"
-						name="ReEnterPassword"
-						minLength="5"
-						value={ReEnterPassword}
-						onChange={(e) => onChange(e)}
-						required
-						
-					/>
-				</div><br/>
-				<input type="submit" className="bttttn" value="Submit" />
-			</form>
-		</div></div>
+					<br/>
+					<input type="submit" className="bttttn" value="Submit" />
+				</form>
+			</div>
+		</div>
 	);
+	}
+	else {
+        window.location.href =`http://localhost:3000/`
+	}
 };
 const mapDispatchToProps=dispatch=>{
     return {
       ChangePassword_action:(formData)=>dispatch(ChangePassword_action(formData)),
     }
-  }
+}
 
 export default connect(null,mapDispatchToProps)(ChangePassword);
