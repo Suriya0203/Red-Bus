11fe2f9e708f47c54cb293e7f09a6f4934b70b1f
Prajeetha R <82923980+Prajeetha313@users.noreply.github.com>
2022-07-15 18:34:38 +0530

    Add files via upload

---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+var mongoose=require('mongoose');
+require('dotenv').config();
+mongoose
+     .connect(process.env.DB, 
+     { useNewUrlParser: true, useUnifiedTopology: true })
+     .then(() => console.log('Database Connected',mongoose.connection.host,mongoose.connection.db))
+     .catch(err => console.log(err));
+
+
+module.exports=mongoose;
