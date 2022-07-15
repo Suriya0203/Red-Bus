ec58a0bd72cbfe1c2282ca1f7e6458e0e1bd77e8
Suriya0203 <suriya0203@gmail.com>
2022-07-15 21:20:27 +0530

    changes

---
+++ client/build/precache-manifest.52a5faa29902469a5c41e1cb2bf3c1e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "75fb4cdb8dbbd7b62b0c",
+    "url": "/static/css/main.f60ba356.chunk.css"
+  },
+  {
+    "revision": "75fb4cdb8dbbd7b62b0c",
+    "url": "/static/js/main.1a9c831b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c0751bb86e7521bce8da",
+    "url": "/static/css/2.68669b52.chunk.css"
+  },
+  {
+    "revision": "c0751bb86e7521bce8da",
+    "url": "/static/js/2.5ce1f6c0.chunk.js"
+  },
+  {
+    "revision": "aa56cdf4edc65ffd9fb3aca0665e235c",
+    "url": "/static/media/login.aa56cdf4.PNG"
+  },
+  {
+    "revision": "c57e7bda5515af0ff9d96a809890f510",
+    "url": "/static/media/redbus.c57e7bda.avif"
+  },
+  {
+    "revision": "9ba80be4f3062ec157cce5cd05e6aabc",
+    "url": "/static/media/tcLogo.9ba80be4.PNG"
+  },
+  {
+    "revision": "0885d70cdebf8884650f68330dbd6ee9",
+    "url": "/index.html"
+  }
+];
